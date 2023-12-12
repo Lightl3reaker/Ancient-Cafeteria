f35b4809cc4d5210fb8e8ed10a5bc95303070b1e
LightBreaker <lightbreaker812@gamil.com>
2023-12-12 22:24:08 +0630

    Updated a little basic
    Added an index,created a base template,tested bootstrap linking, modified some file structures, configured settings.py, created a requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Ancient-Cafeteria
-A cafeteria webproject with django framework.<br>
+ A cafeteria webproject with django framework.<br>
 For study purpose only.<br>
+## Using this repo
+ Plz make installation dependencies from requriements.txt file.
+## Notes for Project Directories
+ Update this readme if you make something with file structure.
+### static
+    This contains bootstap5 local_files.
+### templates
+    global template files for each feature.
+## To-Do List
+  More Comming Soon.:-)
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    BASE_DIR / 'static/',
+    BASE_DIR / 'static/bootstrap/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
