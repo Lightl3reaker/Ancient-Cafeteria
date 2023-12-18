5e86519bbebb041d7b6867bebdd0e1378c8286f7
AncientYoshi <paiswanpyae2002@gmail.com>
2023-12-19 01:26:11 +0630

    Updated

--- myapp/forms.py
+++ myapp/forms.py
@@ -20,4 +20,10 @@ class SignupForm(UserCreationForm):
         fields = ('username','email','password1','password2')
         
     username = forms.CharField(widget=forms.TextInput(
-        attrs={"placeholder": "Your username"}))
+        attrs={"placeholder": "Your username", }))
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={"placeholder": "Your email address",}))
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={"placeholder": "Your password", }))
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={"placeholder": "Confirm password", }))
